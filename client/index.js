64c52e362ba053efc727a3442717880d09bd17b7
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-07-09 20:55:13 -0400

    feat: react app container

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './react/containers/app.js';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
---
+++ client/react/containers/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default () => (
+  <h1>hello world</h1>
+);
+
