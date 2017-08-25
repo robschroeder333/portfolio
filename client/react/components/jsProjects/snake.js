5ae1f0d3f3474fe5181cf32ecaf46f0d3121c1df
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-08-25 17:58:02 -0400

    feat(snake): score, highscore, gamestart, gameover/continue

--- client/react/components/jsProjects/snake.js
+++ client/react/components/jsProjects/snake.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => (
   <div>
     <h1>Snake</h1>
-    <iframe id="snakeFrame" src="snake.html" width="500" height="500"  frameBorder="0" />
+    <h3>Use arrow keys to play (click window first)</h3>
+    <iframe id="snakeFrame" src="snake.html" width="600" height="600"  frameBorder="0" />
   </div>
 )
